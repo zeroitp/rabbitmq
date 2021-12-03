b9f2074dd2ace81dcd9b154f7bf726e8a607cbb8
zeroitp <zeroitp@gmail.com>
2021-12-03 21:37:46 +0700

    Add files via upload

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace RabbitMQ.Consumer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            FanoutExchangeConsumer.Consume(channel);
+            //TopicExchangeConsumer.Consume(channel);
+            //HeaderExchangeConsumer.Consume(channel);
+            //DirectExchangeConsumer.Consume(channel);
+            //QueueConsumer.Consume(channel);
+        }
+    }
+}
