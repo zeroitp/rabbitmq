53056724461e2a5af49ad005a2c1c24b5457c890
zeroitp <zeroitp@gmail.com>
2021-12-03 21:58:53 +0700

    apply RPC pattern for producer

--- RabbitMQ.Producer/Program.cs
+++ RabbitMQ.Producer/Program.cs
@@ -1,23 +1,47 @@
 ﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace RabbitMQ.Producer
 {
     static class Program
     {
-        static void Main(string[] args)
+        //static void Main(string[] args)
+        //{
+        //    var factory = new ConnectionFactory
+        //    {
+        //        Uri = new Uri("amqp://guest:guest@localhost:5672")
+        //    };
+        //    using var connection = factory.CreateConnection();
+        //    using var channel = connection.CreateModel();
+        //    FanoutExchangePublisher.Publish(channel);
+        //    //DirectExchangePublisher.Publish(channel);
+        //    //TopicExchangePublisher.Publish(channel);
+        //    //HeaderExchangePublisher.Publish(channel);
+        //    //QueueProducer.Publish(channel);
+        //}
+
+        public static void Main(string[] args)
         {
-            var factory = new ConnectionFactory
+            Console.WriteLine("RPC Client");
+            for (int i = 0; i < 10; i++)
             {
-                Uri = new Uri("amqp://guest:guest@localhost:5672")
-            };
-            using var connection = factory.CreateConnection();
-            using var channel = connection.CreateModel();
-            FanoutExchangePublisher.Publish(channel);
-            //DirectExchangePublisher.Publish(channel);
-            //TopicExchangePublisher.Publish(channel);
-            //HeaderExchangePublisher.Publish(channel);
-            //QueueProducer.Publish(channel);
+                string message = $"Test RPC {i}";
+                Task t = InvokeAsync(message);
+                t.Wait();
+            }
+            Console.ReadLine();
+        }
+
+        private static async Task InvokeAsync(string message)
+        {
+            var rpcClient = new RpcClient();
+
+            Console.WriteLine($"Requesting message: '{message}' ");
+            var response = await rpcClient.CallAsync(message);
+            Console.WriteLine($"--> Got '{response}'");
         }
     }
 }
---
+++ RabbitMQ.Producer/RPCClient.cs
@@ -0,0 +1,74 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Concurrent;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Producer
+{
+    public class RpcClient
+    {
+        private const string QUEUE_NAME = "rpc_queue";
+
+        private readonly IConnection connection;
+        private readonly IModel channel;
+        private readonly string replyQueueName;
+        private readonly EventingBasicConsumer consumer;
+        private readonly ConcurrentDictionary<string, TaskCompletionSource<string>> callbackMapper =
+                    new ConcurrentDictionary<string, TaskCompletionSource<string>>();
+
+        public RpcClient()
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+            // declare a server-named queue
+            replyQueueName = channel.QueueDeclare(queue: "").QueueName;
+            consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                if (!callbackMapper.TryRemove(ea.BasicProperties.CorrelationId, out TaskCompletionSource<string> tcs))
+                    return;
+                var body = ea.Body.ToArray();
+                var response = Encoding.UTF8.GetString(body);
+                tcs.TrySetResult(response);
+            };
+
+            channel.BasicConsume(
+              consumer: consumer,
+              queue: replyQueueName,
+              autoAck: true);
+        }
+
+        public Task<string> CallAsync(string message, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            IBasicProperties props = channel.CreateBasicProperties();
+            var correlationId = Guid.NewGuid().ToString();
+            props.CorrelationId = correlationId;
+            props.ReplyTo = replyQueueName;
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+            var tcs = new TaskCompletionSource<string>();
+            callbackMapper.TryAdd(correlationId, tcs);
+
+            channel.BasicPublish(
+                exchange: "",
+                routingKey: QUEUE_NAME,
+                basicProperties: props,
+                body: messageBytes);
+
+            cancellationToken.Register(() => callbackMapper.TryRemove(correlationId, out var tmp));
+            return tcs.Task;
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+    }
+}
