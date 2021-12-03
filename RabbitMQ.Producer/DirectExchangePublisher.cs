10fcd2aeae1ffc5fc1e49539348241ccc15282be
zeroitp <zeroitp@gmail.com>
2021-12-03 21:38:48 +0700

    Add files via upload

---
+++ RabbitMQ.Producer/DirectExchangePublisher.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQ.Producer
+{
+    public static class DirectExchangePublisher
+    {
+        public static void Publish(IModel channel)
+        {
+            var ttl = new Dictionary<string, object>
+            {
+                {"x-message-ttl", 30000 }
+            };
+            channel.ExchangeDeclare("demo-direct-exchange", ExchangeType.Direct, arguments: ttl);
+            var count = 0;
+
+            while (true)
+            {
+                var message = new { Name = "Direct Producer", Message = $"Id: { Guid.NewGuid() } request time: { DateTime.Now.ToString("HH:mm:ss:ffff") }" };
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+                channel.BasicPublish("demo-direct-exchange", "account.init", null, body);
+                count++;
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
